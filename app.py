3c88a30c1a5b50fcab99d17e0afbda81b619e34e
MuskaanDev <muskaanwork24@gmail.com>
2024-09-03 19:58:44 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-import cv2
 import os
-from flask import Flask, request, render_template
-from datetime import date
-from datetime import datetime
-import numpy as np
-from sklearn.neighbors import KNeighborsClassifier
-import pandas as pd
+from datetime import date, datetime
+
+import cv2
 import joblib
+import numpy as np
+import pandas as pd
+from flask import Flask, render_template, request
+from sklearn.neighbors import KNeighborsClassifier
 
 app = Flask(__name__)
 
 nimgs = 10
 
-imgBackground=cv2.imread("background.png")
+imgBackground=cv2.imread("background.jpg")
 
 datetoday = date.today().strftime("%m_%d_%y")
 datetoday2 = date.today().strftime("%d-%B-%Y")
